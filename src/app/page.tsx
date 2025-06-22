153c5109a9e0101cba5d37e48c43e4582a6c7453
mohmmedenm7 GitHub <mohmedenym@gmail.com GitHub>
2025-06-23 00:47:21 +0300

    fix: remove unused useState import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Image from 'next/image';
-import { useState } from 'react';
 import Wheel from './components/Wheel';
 import EnvironmentCards from './components/EnvironmentCards';
 
